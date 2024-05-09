7564fd70b1960cd148eb24230a2c8dde85a77899
parkjungwun <165797969+parkjungwun@users.noreply.github.com>
2024-05-09 14:45:54 +0900

    Add files via upload

---
+++ _02_HomeWork.java
@@ -0,0 +1,34 @@
+package Java_Study;
+import java.util.Scanner;
+import java.util.Random;
+
+public class _02_HomeWork {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int score = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int number1 = random.nextInt(90)+10;
+            int number2 = random.nextInt(90)+10;
+
+            System.out.println("다음 두 정수를 더하시오.");
+            System.out.println(number1 + "+" + number2 + " = ?");
+
+            System.out.println("답을 입력하시오.");
+            int answer = scanner.nextInt();
+
+            if ((number1 + number2) == answer) {
+                System.out.println("맞았습니다.");
+                score++;
+            } else {
+                System.out.println("틀렸습니다.");
+
+            }
+        }
+        System.out.println("------------------------------");
+
+        System.out.println("총점은 총 10점 중에서 " + score + "점 입니다.");
+    }
+}
